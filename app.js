1da2847d63e37131bc598029621bfae8e9b73702
Elayne Trimble <34491985+METrimble@users.noreply.github.com>
2022-06-17 18:04:05 -0700

    handlebars test files created, file structure finished

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //reference used: https://www.youtube.com/watch?v=EN6Dx22cPRI&ab_channel=TraversyMedia
 
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2'); //update avoids error relating to connection
+const { engine } = require('express-handlebars');
 
 //Create Connection to mysql
 const db = mysql.createConnection({
@@ -11,14 +12,26 @@ const db = mysql.createConnection({
     database: 'MyCloset'
 });
 
+const app = express();
+
+app.engine('handlebars', engine({ 
+    defaultLayout: "main"
+}));
+app.set('view engine', 'handlebars');
+app.set("views", "./views");
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('test');
+});
+
 db.connect((err) => {
     if(err){
         throw err;
     }
     console.log('MySQL connected..');
 });
-
-const app = express();
 
 app.listen('3000', () => {
     console.log('Server started on port');
