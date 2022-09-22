9b0461de12a4269d5914610a613054b4ccf1d1d1
Elayne Trimble <34491985+METrimble@users.noreply.github.com>
2022-09-22 13:17:49 -0700

    fixed connection to outfitbuilder

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //reference used: https://www.youtube.com/watch?v=EN6Dx22cPRI&ab_channel=TraversyMedia
-
 const express = require('express');
 const mysql = require('mysql2'); //update avoids error relating to connection
 const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
 
 //Create Connection to mysql
 const db = mysql.createConnection({
@@ -14,39 +16,47 @@ const db = mysql.createConnection({
 
 const app = express();
 
+//handlebars
 app.engine('handlebars', engine({ 
 //    defaultLayout: "main"
 }));
 app.set('view engine', 'handlebars');
-app.use(express.static("static"));
-app.use(express.static('public'));
 
-console.log("test in app.js");
-app.use('/outfitbuilder', require('./top.js'));
+
+//static pages
+app.use(express.static("static"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 //load homepage
 app.get('/', function (req, res) {
     res.render('homepage');
 });
 
+app.use('/outfitbuilder', require('./top.js'));
+/*
 //load outfit builder
 app.get('/outfitbuilder', function (req, res) {
     console.log("rendering outfitbuilder");
     res.render('outfitbuilder');
-});
+});*/
 
-//load outfit editor
+//load outfit viewer
 app.get('/outfitviewer', function (req, res) {
     res.render('outfitviewer');
 });
 
+
+
 //connect mysql
-/*db.connect((err) => {
+db.connect((err) => {
     if(err){
         throw err;
     }
     console.log('MySQL connected..');
-});*/
+});
 
 //listen on port 3000
 app.listen('3000', () => {
--- top.js
+++ top.js
@@ -1,9 +1,9 @@
-module.exports = function(){
+/*module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
     function getTops(res, mysql, context, complete){
-        /*mysql.pool.query("SELECT t.ID, t.Type, t.Color, i.path AS path, zi.path AS zpath FROM top AS t, image AS i, zoomed_image AS zi;", function(error, results, fields){
+        mysql.pool.query("SELECT t.ID, t.Type, t.Color, i.path AS path, zi.path AS zpath FROM top AS t, image AS i, zoomed_image AS zi;", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 console.log(JSON.stringify(error));
@@ -11,14 +11,14 @@ module.exports = function(){
             }
             context.top = results;
             complete();
-        });*/
+        });
     }
 
     router.get('/', function(req, res){
         console.log("test1");
         var callbackCount = 0;
         var context = {};
-        /*var mysql = req.app.get('mysql');
+        var mysql = req.app.get('mysql');
         console.log("test2");
         getTops(res, mysql, context, complete);
         console.log("test3");
@@ -28,8 +28,17 @@ module.exports = function(){
             if(callbackCount >= 1){
                 res.render('top', context);
             }
-        }*/
+        }
     });
 
     return router;
-};
+}; */
+
+const express = require('express');
+var router = express.Router();
+module.exports = router;
+
+router.get('/', function(req, res){
+    console.log("here");
+    res.render('outfitbuilder');
+});
